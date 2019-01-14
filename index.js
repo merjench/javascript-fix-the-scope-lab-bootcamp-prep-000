3ff282247f14faa3b6d7661f98c5322d985caed7
Merjen Charyyeva <merjen.chary@gmail.com>
2019-01-14 20:00:22 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,29 @@
+var animal = 'dog'
+
+function myAnimal() {
+  return animal
+}
+
+function yourAnimal() {
+var animal = 'cat';
+  return animal
+}
+
+function add2(n) {
+  var two= 2
+  return n + two;
+}
+
+var funkyFunction = function() {
+  return function() {
+    return "FUNKY!"
+  }
+}
+
+var funkyFunction = function() {
+    return function(){
+      return "FUNKY!"
+    }
+  }
+
+   var theFunk = funkyFunction()()
